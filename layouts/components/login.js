116a7fc4a52d5c1e19981b818891e1b61125f5b9
ElizabethOgbee <ogbeititi345@gmail.com>
2023-02-01 10:03:35 +0100

    email use is 'hello@gmail.com' for testing purpose

--- layouts/components/login.js
+++ layouts/components/login.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from 'next/router'
+import Image from "next/image";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   let router= useRouter();
@@ -15,60 +16,87 @@ const Login = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (username === "admin" && password === "password") {
+    if (email === "hello@example.com") {
       setError("");
       // Navigate to the home page after successful login
       router.push('/home')
     } else {
-      setError("Invalid username or password");
+      setError("Invalid email or password");
     }
   };
 
   return (
-    <form className="bg-white p-6 rounded-lg shadow-md" onSubmit={handleSubmit}>
-      <h2 className="text-lg font-medium mb-4">Login</h2>
-      <div className="mb-4">
+    <div className="flex justify-center items-center text-center h-screen bg-theme-light">
+      <form className="bg-white  rounded-lg shadow-md px-12 py-6 " onSubmit={handleSubmit}>
+      <div className="flex gap-4 justify-center">
+      <img src="images/favicon.png" alt="spplychain-favicon" className='w-[30px] h-[30px]' />
+      <h2 className="text-3xl font-semi-bold mb-4 "
+      >
+        Splychain
+        </h2>
+      </div>
+      <div className="">
+        <h2 className="text-lg text-center">
+          Sign in
+        </h2>
+        <p>Continue with</p>
+        <div className="py-4">
+          <ul className="flex justify-center gap-2">
+            <li className="cursor-pointer">
+            <Link href='' />
+            <img src="images/twitter.svg" alt="twitter-icon" />
+            </li>
+            <li className="cursor-pointer">
+            <Link href='' />
+            <img src="images/google.svg" alt="google-icon"/>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="mb-4 ">
         <label
           className="block text-gray-700 font-medium mb-2"
-          htmlFor="username"
+          htmlFor="email"
         >
-          Username
+          Email
         </label>
         <input
           type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          id="email"
+          name="email"
+          placeholder="hello@example.com"
+          value={email}
+          onChange={(e) => setEmail
+        (e.target.value)}
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white"
         />
       </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 font-medium mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-        />
-      </div>
-      {error && (
-        <div className="mb-4 text-red-500 text-sm">{error}</div>
+       {error && (
+        <div className="mb-3 text-red-500 text-sm">{error}</div>
+        
       )}
       <button
         type="submit"
-        className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="bg-blue-900 text-white appearance-none border-2 rounded-full cursor-pointer w-full py-2 px-4 leading-tight focus:outline-none"
       >
-        Login
+        Continue with Email
       </button>
-    </form>
+      {/* <div className="mb-4">
+        </div> */}
+      <div className="mb-1 mt-4">
+        <label
+          className="block text-gray-700 font-medium "
+          htmlFor="password"
+        >
+          External Wallet
+        </label>
+      </div>
+      <button className="bg-blue-900 text-white appearance-none border-2 rounded-full cursor-pointer w-full py-2 px-4 leading-tight focus:outline-none"
+        >
+          Connect with Wallet
+          </button>
+    </form> 
+    </div>
   );
 };
 
